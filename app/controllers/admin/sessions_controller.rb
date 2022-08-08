fe040a16b4ce3e54966cbf078945bb7b9f6e8d51
JeromeReyes1999 <rjerome415@gmail.com>
2022-08-08 12:33:14 +0800

    [Feat #1] fix bug, user can't try email without password

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,12 +1,9 @@
 class Admin::SessionsController < Devise::SessionsController
-  def create
-    user = User.find_by_email(params[:admin_user][:email])
 
-    if user.nil?
-      flash[:notice] = "account doesn't exist"
-      redirect_to action: :new
-    elsif user.client?
-      flash[:notice] = "you don't have permission"
+  def create
+    user = User.find_for_authentication(email: params[:admin_user][:email])
+    if user&.client? && user&.valid_password?(params[:admin_user][:password])
+      flash[:notice] = "You don't have permission"
       redirect_to action: :new
     else
       super
