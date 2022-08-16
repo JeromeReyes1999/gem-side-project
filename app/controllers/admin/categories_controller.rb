00db827f1ac9ec3b084ac22f48f163b47ba585f6
JeromeReyes1999 <rjerome415@gmail.com>
2022-08-16 14:44:23 +0800

    [Feature 12] add Category model

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,48 @@
+class Admin::CategoriesController < ApplicationController
+    before_action :set_category, only: [:edit, :update, :destroy]
+
+    layout 'dashboard_layout'
+
+    def index
+      @categories= Category.all
+    end
+
+    def new
+      @category = Category.new
+    end
+
+    def edit; end
+
+    def update
+      if @category.update(category_params)
+        redirect_to admin_categories_path
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      unless @category.destroy
+        flash[:alert] = "this disaster type is currently in use in a post"
+      end
+      redirect_to admin_categories_path
+    end
+
+    def create
+      @category = Category.new(category_params)
+      if @category.save
+        redirect_to admin_categories_path
+      else
+        render :new
+      end
+    end
+
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
+
+    def set_category
+      @category = Category.find(params[:id])
+    end
+end
