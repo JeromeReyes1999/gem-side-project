da00142a60ffeae69288222910f5d133616961a5
JeromeReyes1999 <rjerome415@gmail.com>
2022-08-16 14:46:14 +0800

    [Feature 12] add Category model

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,8 @@ class Item < ApplicationRecord
   validates_presence_of :start_at
   enum status: [:Active, :Inactive]
 
+  belongs_to :category
+
   mount_uploader :image, ImageUploader
   default_scope { where(deleted_at: nil) }
 
