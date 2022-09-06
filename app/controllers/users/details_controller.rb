12c5e5901414601d416bf87d7f8157aecaffea26
JeromeReyes1999 <rjerome415@gmail.com>
2022-09-06 11:03:34 +0800

    [Feature 23] Client Profile Page

--- app/controllers/users/details_controller.rb
+++ app/controllers/users/details_controller.rb
@@ -1,5 +1,11 @@
 class Users::DetailsController < ApplicationController
   before_action :authenticate_user!
 
-  def show; end
+  def show
+    @orders = Order.where(user: current_user) if params[:activities] == 'orders' || params[:activities].blank?
+    @invitations = User.where(parent: current_user) if params[:activities] == 'invites'
+    @bets = Bet.includes(:item).where(user: current_user) if params[:activities] == 'gacha'
+    @winners = Winner.includes(:item).where(user: current_user).won if params[:activities] == 'winning'
+  end
+
 end
