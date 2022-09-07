d8e289a67d36dc387bf74554a8f0c723a9d97eee
JeromeReyes1999 <rjerome415@gmail.com>
2022-09-07 11:03:30 +0800

    add helper for email concealment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def concat_address(address)
     "#{address.street_address}, #{address.barangay.name}, #{address.city.name}, #{address.province.name}, #{address.region.name}"
   end
+  def conceal_email(email)
+    email[2..-3] = '*' * (email.size-4)
+    email
+  end
 end
