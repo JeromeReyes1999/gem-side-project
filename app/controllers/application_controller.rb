823d4ad9fc901591b42c4e67c47e387fd176820c
JeromeReyes1999 <rjerome415@gmail.com>
2022-09-13 17:03:27 +0800

    clean code, edit bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   layout 'home_layout'
-  before_action :set_banners
 
-  def set_banners
-    @banners = Banner.where('online_at <= ? AND offline_at > ?', Time.now, Time.now).active
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,17 @@ module ApplicationHelper
   def concat_address(address)
     "#{address.street_address}, #{address.barangay.name}, #{address.city.name}, #{address.province.name}, #{address.region.name}"
   end
+
   def conceal_email(email)
     email[2..-3] = '*' * (email.size-4)
     email
+  end
+
+  def banners
+    Banner.where('online_at <= ? AND offline_at > ?', Time.now, Time.now).active
+  end
+
+  def news_tickers
+    NewsTicker.active.limit(5)
   end
 end
