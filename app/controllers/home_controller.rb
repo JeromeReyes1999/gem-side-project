ab1173f3bddfa0e9ae28969319fda83f51b5c649
JeromeReyes1999 <rjerome415@gmail.com>
2022-09-13 14:12:57 +0800

    [Feature 35] Client Homepage view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @winners = Winner.includes(:user).published.first(2)
+    @winners = Winner.includes(:user).published.order(id: :desc)
+    @items = Item.active.starting.order(id: :desc)
   end
 end
